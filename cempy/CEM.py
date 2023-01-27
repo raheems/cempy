ea6d56461ae64825e4770ca2a990191bb031c470
raheems <raheem@gmail.com>
2023-01-27 11:17:46 -0500

    project reorganized with cookiecutter

--- cempy/CEM.py
+++ cempy/CEM.py
@@ -1,3 +1,4 @@
+"""Main module."""
 # Load the class
 
 # CEM with Python
@@ -15,35 +16,64 @@ from scipy import stats
 
 class CEM:
 
-    def __init__(self, data, treatment, metrics=[], matchvar=[], exclude=[]):
-        self.matchvar = [i for i in matchvar if i not in exclude + metrics]
+    """
+    Creates a new CEM object. This is the first step to performing Coarsened Exact Machine using this package.
+    ...
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+
+
+    """
+
+    def __init__(self, data, trt, metric=[], matching_on=[], exclude=[]):
+
+        """
+        Parameters
+        ----------
+        data: DataFrame
+            Pandas data frame
+        trt: str
+            The name of the trt variable as appears in the DataFrame
+        metric: list
+            List of column names that are of primary interest to perform evaluation about
+        matching_on: List
+            List of columns names on which the matching will be performed
+        exclude: List, optional
+            List of column names that are to be excluded from the analysis, but they will still be retained in the data
+
+        """
+        self.matching_on = [i for i in matching_on if i not in exclude + metric]
 
         # Data cleaning process
-        # Remove missing values if found in matchvar
+        # Remove missing values if found in matching_on
 
-        self.data = data.dropna(subset=matchvar)
-        self.treatment = treatment
-        self.metrics = metrics
+        self.data = data.dropna(subset=matching_on)
+        self.trt = trt
+        self.metric = metric
 
-        # Denifing the bin signatures
-        self.data['bin_sig'] = self.data[self.matchvar[0]].astype(str)
-        for i in range(1, len(self.matchvar)):
-            self.data['bin_sig'] = self.data['bin_sig'].astype(str) + '_' + self.data[self.matchvar[i]].astype(str)
+        # Create bin markers by concatenating the values of `matching_on`
+        self.data['bin_marker'] = self.data[self.matching_on[0]].astype(str)
+        for i in range(1, len(self.matching_on)):
+            self.data['bin_marker'] = self.data['bin_marker'].astype(str) + '_' + self.data[self.matching_on[i]].astype(str)
 
-        # Define treatment control dataframes
-        self.t = self.data.loc[self.data[treatment] == 1]
-        self.c = self.data.loc[self.data[treatment] == 0]
+        # Define trt control dataframes
+        self.t = self.data.loc[self.data[trt] == 1]
+        self.c = self.data.loc[self.data[trt] == 0]
 
         # Other attributes
 
         # CEM summary table
-        self.ms = self.cem_summary(self.treatment)
+        self.ms = self.summary(self.trt)
         self.nt_matched = self.ms.groupby(['is_matched']).sum().iloc[1, 0]
         self.nc_matched = self.ms.groupby(['is_matched']).sum().iloc[1, 1]
 
         # CEM summary with weights for each record
         self.msw = self.get_weights()
-        self.msw[self.treatment] = 0
+        self.msw[self.trt] = 0
         self.mt = self.matched_table()
 
         # Data with weights
@@ -52,24 +82,29 @@ class CEM:
 
     # Return the list of matched variables
     def matched_var(self):
-        return list(self.matchvar)
+
+        """
+        Returns a list of variables used to perform matching
+
+        """
+        return list(self.matching_on)
 
 
     # Create CEM summary
 
-    def cem_summary(self, treatment=False):
+    def summary(self, treatment=False):
         """
-        cem_summary method performes many-to-many matching and returns a copy of the original `dataframe` that
+        summary method performs many-to-many matching and returns a copy of the original `dataframe` that
         contains addition column called 'is_matched' which indicates whether a member has a match or not
         """
 
         # Group members by bin signature andc ount how many members belong to each type
 
-        self.t_grouped = self.t.groupby(['bin_sig']).size().to_frame(name='trt_count').reset_index()
-        self.c_grouped = self.c.groupby(['bin_sig']).size().to_frame(name='ctl_count').reset_index()
+        self.t_grouped = self.t.groupby(['bin_marker']).size().to_frame(name='trt_count').reset_index()
+        self.c_grouped = self.c.groupby(['bin_marker']).size().to_frame(name='ctl_count').reset_index()
 
         # Combine all grouped members into a single table
-        self.member_summary = pd.merge(self.t_grouped, self.c_grouped, how='outer', on=['bin_sig'])
+        self.member_summary = pd.merge(self.t_grouped, self.c_grouped, how='outer', on=['bin_marker'])
 
         # Replace NAs with 0
         self.member_summary = self.member_summary.fillna(0)
@@ -83,45 +118,58 @@ class CEM:
 
 
     def get_matched(self):
+
+        """
+        Returns the matched dataframe
+        """
+
         # 1. Matching
-        self.matched_data = self.cem_summary(self.treatment)
+        self.matched_data = self.summary(self.trt)
 
         # Get a list of bins that are matched
-        bin_matched = self.matched_data.loc[self.matched_data['is_matched'] == 1]['bin_sig']
+        bin_matched = self.matched_data.loc[self.matched_data['is_matched'] == 1]['bin_marker']
         bin_matched = list(bin_matched)
 
         # Get the matched members out
-        data_matched = self.data[self.data['bin_sig'].isin(bin_matched)]
+        data_matched = self.data[self.data['bin_marker'].isin(bin_matched)]
 
         return data_matched
 
 
     def get_unmatched(self):
+
+        """
+        Returns the unmatched dataframe
+        """
+
         # 1. Matching
-        self.matched_data = self.cem_summary(self.treatment)
+        self.matched_data = self.summary(self.trt)
 
         # Get a list of bins that are matched
-        bin_unmatched = self.matched_data.loc[self.matched_data['is_matched'] == 0]['bin_sig']
+        bin_unmatched = self.matched_data.loc[self.matched_data['is_matched'] == 0]['bin_marker']
         bin_unmatched = list(bin_unmatched)
 
         # Get matched members out
-        data_unmatched = self.data[self.data['bin_sig'].isin(bin_unmatched)]
+        data_unmatched = self.data[self.data['bin_marker'].isin(bin_unmatched)]
 
         return data_unmatched
 
 
     def matched_table(self):
         """
-        Returns a table of matched or unmatched members in the treatment and control groups
+        Returns a table of matched or unmatched members in the trt and control groups
         """
 
-        summary_matched = self.cem_summary(self.treatment)[['trt_count', 'ctl_count', 'is_matched']]
+        summary_matched = self.summary(self.trt)[['trt_count', 'ctl_count', 'is_matched']]
 
         return summary_matched.groupby(['is_matched']).sum()
 
 
     def match_rate_out(self):
-        # Calculate matching rates: overall, treatment and control
+
+        """
+        Calculate matching rates: overall, trt and control
+        """
 
         overall = (self.mt.iloc[1].sum()) / (self.mt.sum().sum())
         treatment = (self.mt.iloc[1, 0].sum()) / (self.mt.iloc[:, 0].sum())
@@ -131,11 +179,12 @@ class CEM:
 
 
     def get_weights(self):
-        """
-        CEM summary with weight column added
-        """
 
-        # Matched control members get normalized weights according to treatment group
+        """
+        Returns a dataframe with CEM summary along with bin_markers, treatment counts,
+        control counts, a flag whether matched was found and the weight.
+        """
+        # Matched control members get normalized weights according to trt group
         members_summary_matched = self.ms.loc[(self.ms['is_matched'] > 0)]
         members_summary_matched['wt'] = (members_summary_matched['trt_count'] / members_summary_matched['ctl_count']) * \
                                         (self.nc_matched / self.nt_matched)
@@ -144,19 +193,25 @@ class CEM:
 
 
     def get_weighted_data(self):
-        dfweighted = self.get_weights()
+
+        """
+        Returns a dataframe with the calculated weights added as a column.
+        Treatment rows will receive a wt = 1 while control will receive a calculated weight
+
+        """
+        df_weighted = self.get_weights()
         data_matched = self.get_matched()
 
         todrop = ['trt_count', 'ctl_count', 'is_matched']
-        dfweighted = dfweighted.drop(columns=todrop)
+        df_weighted = df_weighted.drop(columns=todrop)
 
         # Match corresponding weight for every member
-        data_matched['bin_sig'] = data_matched['bin_sig'].astype(str)
-        dfweighted['bin_sig'] = dfweighted['bin_sig'].astype(str)
-        data_matched_w_weights = pd.merge(data_matched, dfweighted, how='left', on=['bin_sig'])
+        data_matched['bin_marker'] = data_matched['bin_marker'].astype(str)
+        df_weighted['bin_marker'] = df_weighted['bin_marker'].astype(str)
+        data_matched_w_weights = pd.merge(data_matched, df_weighted, how='left', on=['bin_marker'])
 
         # Treatment cases get a weight of 1
-        data_matched_w_weights.loc[data_matched_w_weights[self.treatment] == 1, 'wt'] = 1
+        data_matched_w_weights.loc[data_matched_w_weights[self.trt] == 1, 'wt'] = 1
         data_matched_w_weights = data_matched_w_weights.reset_index(drop=True)
 
         return data_matched_w_weights
@@ -167,38 +222,44 @@ class CEM:
         Internal function for use within the metric_summary()
         """
 
-        temp = self.data[[self.treatment, 'bin_sig', metric]]
-        temp = temp.groupby(['bin_sig', self.treatment]).sum().reset_index()
-        effect = pd.merge(temp, self.msw, how='left', on=['bin_sig'])
+        df_tmp = self.data[[self.trt, 'bin_marker', metric]]
+        df_tmp = df_tmp.groupby(['bin_marker', self.trt]).sum().reset_index()
+        effect = pd.merge(df_tmp, self.msw, how='left', on=['bin_marker'])
         effect = effect.fillna(0)
 
-        # Treatment effect
-        treatment_effect = ((effect[self.treatment + '_x'] * effect['is_matched'] * effect[metric]).sum()) / self.nt_matched
-        control_effect = ((1 - effect[self.treatment + '_x']) * effect['is_matched'] * effect[metric] * \
-                          effect['wt']).sum() / self.nc_matched
+        # Calculate effect for the treatment and control groups
+        trt_effect = ((effect[self.trt + '_x'] * effect['is_matched'] * effect[metric]).sum()) / self.nt_matched
+        ctl_effect = ((1 - effect[self.trt + '_x']) * effect['is_matched'] * effect[metric] * \
+                      effect['wt']).sum() / self.nc_matched
 
-        return (treatment_effect, control_effect)
+        return (trt_effect, ctl_effect)
 
 
     def metric_summary(self, metrics=False):
-        if not metrics:
-            metrics = self.metrics
 
-        # Remove constant 0 columns from the list of metrics to run
+        """
+        Returns a summary table for the metrics after matching is performed.
+        Shows estimated impact of each metrics, their 95% normal confidence intervals
+
+        """
+        if not metrics:
+            metrics = self.metric
+
+        # Remove constant 0 columns from the list of metric to run
         # data_matched_weights = self.get_weighted_data()
         # mt = self.matched_table()
 
         Control = 'Control'
 
         i = 1
-        results = pd.DataFrame(
+        metric_output = pd.DataFrame(
             columns=[
                 'Treatment'
-                , 'Matched Treatment group - size'
-                , 'Treatment group - match rate'
+                , 'Treatment group - Matched (n)'
+                , 'Treatment group - Match rate'
                 , 'Control'
-                , 'Matched Control group - size'
-                , 'Control group - match rate'
+                , 'Control group - Matched (n)'
+                , 'Control group - Match rate'
                 , 'Metric'
                 , 'Metric (Treatment)'
                 , 'Metric (Treatment) LCF'
@@ -206,25 +267,25 @@ class CEM:
                 , 'Metric (Control)'
                 , 'Metric (Control) LCF'
                 , 'Metric (Control) UCF'
-                , 'Impact'
-                , 'Impact (LCF)'
-                , 'Impact (UCF)'
-                , 'Impact (%)'
-                , 'Impact (%) LCF'
-                , 'Impact (%) UCF'
+                , 'Metric Impact'
+                , 'Metric Impact (LCF)'
+                , 'Metric Impact (UCF)'
+                , 'Metric Impact (%)'
+                , 'Metric Impact (%) LCF'
+                , 'Metric Impact (%) UCF'
                 , 'p-value'
             ]
         )
 
-        control_weights = self.weighted_data.loc[self.weighted_data[self.treatment] == 0, 'wt']
-        control_n = (self.weighted_data[self.treatment] == 0).sum()
-        treatment_n = (self.weighted_data[self.treatment] == 1).sum()
+        control_weights = self.weighted_data.loc[self.weighted_data[self.trt] == 0, 'wt']
+        control_n = (self.weighted_data[self.trt] == 0).sum()
+        treatment_n = (self.weighted_data[self.trt] == 1).sum()
 
         for metric in metrics:
 
             effect = self._effect_out(metric)
 
-            control_metric = self.weighted_data.loc[self.weighted_data[self.treatment] == 0, metric]
+            control_metric = self.weighted_data.loc[self.weighted_data[self.trt] == 0, metric]
             control_mean = np.average(control_metric, weights=control_weights)
             control_std = np.sqrt(np.average((control_metric - control_mean) ** 2, weights=control_weights))
 
@@ -235,7 +296,7 @@ class CEM:
                 control_LCI = 0.00001
                 control_UCI = 0.00001
 
-            treatment_metric = self.weighted_data.loc[self.weighted_data[self.treatment] == 1, metric]
+            treatment_metric = self.weighted_data.loc[self.weighted_data[self.trt] == 1, metric]
             treatment_mean = treatment_metric.mean()
             treatment_std = np.sqrt(((treatment_metric - treatment_mean) ** 2).mean())
 
@@ -247,14 +308,14 @@ class CEM:
                 treatment_UCI = 0.00001
 
             ttest_p_value = statsmodels.stats.weightstats.ttest_ind(
-                x1=self.weighted_data.loc[self.weighted_data[self.treatment] == 1, metric],
-                x2=self.weighted_data.loc[self.weighted_data[self.treatment] == 0, metric],
+                x1=self.weighted_data.loc[self.weighted_data[self.trt] == 1, metric],
+                x2=self.weighted_data.loc[self.weighted_data[self.trt] == 0, metric],
                 alternative='two-sided',
-                weights=(self.weighted_data.loc[self.weighted_data[self.treatment] == 1, 'wt'],
-                         self.weighted_data.loc[self.weighted_data[self.treatment] == 0, 'wt']))[1]
+                weights=(self.weighted_data.loc[self.weighted_data[self.trt] == 1, 'wt'],
+                         self.weighted_data.loc[self.weighted_data[self.trt] == 0, 'wt']))[1]
 
-            results.loc[i] = [
-                self.treatment
+            metric_output.loc[i] = [
+                self.trt
                 , int(round(self.mt.iloc[:, 0].sum() * self.match_rate_out()[1]))
                 , self.match_rate_out()[1]
                 , Control
@@ -284,4 +345,4 @@ class CEM:
             ]
             i = + i + 1
 
-        return results
+        return metric_output
--- setup.py
+++ setup.py
@@ -1,13 +1,49 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
 from setuptools import setup, find_packages
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0', ]
+
+test_requirements = ['pytest>=3', ]
+
 setup(
-   name='cempy',
-   version='0.0.1',
-   author='Enayetur Raheem',
-   author_email='eraheem@dataskool.com',
-   packages=find_packages(),
-   url='http://github.com/raheems/cempy',
-   license='LICENSE.md',
-   description='Coarsened Exact Matching with Python',
-   long_description=open('README.md').read()
+    author="Enayetur Raheem",
+    author_email='eraheem@dataskool.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="CEM with Python",
+    entry_points={
+        'console_scripts': [
+            'cempy=cempy.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='cempy',
+    name='cempy',
+    packages=find_packages(include=['cempy', 'cempy.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/raheems/cempy',
+    version='0.0.1',
+    zip_safe=False,
 )
